ffad9736431722c4ba1b6f72df4ffc0982a5161b
Eloeloell <giletafranek@gmail.com>
2025-08-24 11:59:27 +0200

    Optymalizacja CSS + Dodanie zdjec Poscigu

--- poscig-strona/script.js
+++ poscig-strona/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("components/header.html")
-    .then(res => res.text())
-    .then(data => document.getElementById("header").innerHTML = data);
+  const loadComponent = (id, file) => {
+  fetch(file)
+    .then(res => res.ok ? res.text() : Promise.reject(res))
+    .then(data => document.getElementById(id).innerHTML = data)
+    .catch(() => console.error('Nie udalo sie zaladowac ${file}'));
+};
 
-  fetch("components/footer.html")
-    .then(res => res.text())
-    .then(data => document.getElementById("footer").innerHTML = data);
+  loadComponent("header", "components/header.html");
+  loadComponent("header", "components/header.html");
 });
